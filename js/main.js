ef3046f8d22e29a086ad2253d4bef268c0a3ac87
MyKetapang Group <myketapang@gmail.com>
2018-10-26 17:02:17 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -229,7 +229,7 @@
 			$.ajax({      	
 
 		      type: "POST",
-		      url: "inc/sendEmail.php",
+		      url: "myketapang.github.io/acohps2019/inc/sendEmail.php",
 		      data: $(form).serialize(),
 		      beforeSend: function() { 
 
